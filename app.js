b350f91e3cff917e44c3b663fd248bf3afedda94
zhangjing <505965104@qq.com>
2019-06-20 11:29:42 +0800

    处理url

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const Koa = require('koa');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
 app.use(async (ctx,next)=>{
-    ctx.response.head  = "text/html";
-    ctx.response.body = "Hello"
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
+    await next();
 });
+router.get('/hello/:name',async (ctx,next)=>{
+    var name = ctx.params.name;
+    ctx.response.body = `hello,${name}`;
+});
+router.get('/',async(ctx,next)=>{
+    ctx.response.body = `<h1>Index</h1><form action="/signin" method="post">
+        <p>Name:<input name="name" value="koa"></p>
+        <p>Password:<input name="password" type="password"></p>
+        <p><input type="submit" value="Submit"></p>
+    </form>`;
+});
+router.post('/signin',async (ctx,next)=>{
+    var name = ctx.request.body.name||'',
+        password = ctx.request.body.password||'';
+    console.log(`signin with name:${name},password:${password}`);
+    if(name==='koa'&&password==='123456'){
+        ctx.response.body = `Welcome,${name}`;
+    }else{
+        ctx.response.body='<h1>Login failed!</h1><a href="/">Try again</a>'
+    }
+
+})
+app.use(bodyParser());
+app.use(router.routes());
+
 app.listen(3030);
 console.log('app is running at 3030');
